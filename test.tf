7f1906f09dcd0c9a542feb4e57d2ddc22b73d3b3
Vasilyev, Viacheslav <viacheslav.vasilyev@accenture.com>
2020-03-14 16:46:53 +0200

    show sts info

---
+++ test.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  version = ">= 2.28.1"
+  region  = "eu-west-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = "${data.aws_caller_identity.current.account_id}"
+}
+
+output "caller_arn" {
+  value = "${data.aws_caller_identity.current.arn}"
+}
+
+output "caller_user" {
+  value = "${data.aws_caller_identity.current.user_id}"
+}
